c5b21550e945058ec5a326d3432c890f95de8746
Flávio Miranda <nascimentoflavio@hotmail.com>
2023-12-09 08:44:14 -0300

    Correção do script de controle do menu lateral.

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,23 +3,11 @@ const menu1 = document.getElementById("user")
 
 botao.addEventListener('click', function () {
     const menu = document.getElementById('lateral')
-    if (temClasse(menu, 'sumir')) {
-        menu.classList.remove('sumir')
-    } else {
-        menu.classList.add('sumir')
-    }
+    menu.classList.toggle('sumir')
 })
 
 
 menu1.addEventListener('click', function (evento) {
     const janela = document.getElementById('menu-1')
-    if (temClasse(janela, "ocultar")){
-        janela.classList.remove("ocultar")
-    }else{
-        janela.classList.add("ocultar")
-    }
+    janela.classList.toggle("ocultar")
 })
-
-function temClasse(el, classe) {
-    return (el.className.split(' ').indexOf(classe) + 1);
-}
